c38aa6149a54c2f4ca302121f7325e97f0d58ece
Marcos Luna <marcosluna87.dev@gmail.com>
2026-05-20 12:30:29 -0300

    feat: integrate swagger-ui-express to serve documentation at /api-docs and enable cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import express from "express";
+import cors from "cors";
+import YAML from "yamljs";
+import swaggerUi from "swagger-ui-express";
 import router from "./routes/index.js";
 import { notFound } from "./helpers/errors.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
+// Habilitamos CORS de forma global para desarrollo
+app.use(cors());
+
+// Cargar la documentación
+const swaggerDocument = YAML.load("./openapi.yaml");
+
 app.use(express.json());
+
+// 3. Documentación (Debe ir ANTES del 404)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(router);
 
